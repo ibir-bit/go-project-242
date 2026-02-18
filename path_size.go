213acc04d5478b6d6219d810b336ad223ce2ebf8
Ivan Bir <ivbir.job@gmail.com>
2026-02-18 18:02:24 +0000

    New code

--- path_size.go
+++ path_size.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -12,24 +13,25 @@ func main() {
 }
 
 func GetSize(adress string) (int64, error) {
-	// Проверяем существование пути
+	var human bool
+	flag.BoolVar(&human, "h", false, "human-readable sizes (auto-select unit)")
+
+	flag.Parse()
+
 	_, err := os.Lstat(adress)
 	if err != nil {
 		return 0, errors.New("Такого файла не существует")
 	}
 
-	// Получаем информацию о целевом файле (переходя по ссылкам)
 	targetInfo, err := os.Stat(adress)
 	if err != nil {
 		return 0, fmt.Errorf("не могу получить информацию: %w", err)
 	}
 
-	// Если это файл - возвращаем размер
 	if !targetInfo.IsDir() {
 		return targetInfo.Size(), nil
 	}
 
-	// Читаем содержимое директории (os.ReadDir автоматически работает с симлинками)
 	entries, err := os.ReadDir(adress)
 	if err != nil {
 		return 0, errors.New("Такого пути не существует")
@@ -37,16 +39,13 @@ func GetSize(adress string) (int64, error) {
 
 	var size int64
 
-	// Проходим по всем файлам в директории
 	for _, entry := range entries {
 		fullPath := filepath.Join(adress, entry.Name())
 
-		// Пропускаем поддиректории
 		if entry.IsDir() {
 			continue
 		}
 
-		// Получаем информацию о файле (переходя по ссылкам)
 		info, err := os.Stat(fullPath)
 		if err != nil {
 			return 0, errors.New("Ошибка чтения")
@@ -56,4 +55,34 @@ func GetSize(adress string) (int64, error) {
 	}
 
 	return size, nil
+}
+
+func FormatSize(human bool, size int64) string {
+	if human {
+		fmt.Println(HumanizeSize(size))
+	} else {
+		fmt.Println(size)
+	}
+}
+
+func HumanizeSize(bytes int64) string {
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+		TB = GB * 1024
+	)
+
+	switch {
+	case bytes >= TB:
+		return fmt.Sprintf("%.2fTB", float64(bytes)/TB)
+	case bytes >= GB:
+		return fmt.Sprintf("%.2fGB", float64(bytes)/GB)
+	case bytes >= MB:
+		return fmt.Sprintf("%.2fMB", float64(bytes)/MB)
+	case bytes >= KB:
+		return fmt.Sprintf("%.2fKB", float64(bytes)/KB)
+	default:
+		return fmt.Sprintf("%dB", bytes)
+	}
 }
